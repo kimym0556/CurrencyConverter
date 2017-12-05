0f84d1dd233fea7cb1f225f976c54dc8cfd67854
Yumi Kim <yumikim@Yumis-MacBook.local>
2017-12-04 19:13:53 -0800

    First commit

---
+++ currencyconverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  currencyconverter
+//
+//  Created by YUMI KIM on 2017. 12. 4..
+//  Copyright © 2017년 YUMI KIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
